cada22822a77922e6cd40e148bac1d4dd896c139
Rafał Rojek <RafalR@advaoptical.com>
2019-09-14 16:57:55 +0200

    add redux-persist, persisting cart from reducer into the local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
 import {createStore, applyMiddleware} from "redux";
 import logger from 'redux-logger';
-
+import {persistStore} from 'redux-persist'; //allows the browser to cache the store
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+//apply the redux reducers with the middleware - logging 
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+//apply persistence in the local or session storage
+export const persistor = persistStore(store);
 
-export default store;
+export default {store, persistor}; //one can return the object with both stuff 
