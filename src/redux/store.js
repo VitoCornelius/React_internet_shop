50d030c51287965ce345c3451f13f5259aac705c
Rafał Rojek <RafalR@advaoptical.com>
2019-09-23 20:25:53 +0200

    remove redux logger from heroku build

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,11 @@ import logger from 'redux-logger';
 import {persistStore} from 'redux-persist'; //allows the browser to cache the store
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+}
 
 //apply the redux reducers with the middleware - logging 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
